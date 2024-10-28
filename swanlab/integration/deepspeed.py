81194d8280c16884983f83c72bf573c8823b8c74
ShaohonChen <shaohon_chen@115lab.club>
2024-10-28 21:11:47 +0800

    add deepspeed integration

--- swanlab/integration/deepspeed.py
+++ swanlab/integration/deepspeed.py
@@ -23,16 +23,57 @@ accelerator = Accelerator(log_with=[tracker, "all"])
 """
 
 import os
-from typing import Any, Dict, List, Optional, Union
+from abc import ABC, abstractmethod
 
 import swanlab
 
 
 try:
-    import deepspeed
+    from deepspeed.monitor.monitor import Monitor
+    import deepspeed.comm as dist
+    from deepspeed.runtime.config import DeepSpeedConfig
 except ImportError:
     raise RuntimeError(
         "This contrib module requires deepspeed to be installed. \n"
         "Please install it with command: \n\t pip install accelerate\n"
-        "see more detail from: https://www.deepspeed.ai/getting-started/"
+        "Find more detail at: https://www.deepspeed.ai/getting-started/"
     )
+
+
+class SwanlabMonitor(Monitor):
+
+    def __init__(self, monitor_config):
+        swanlab_config = monitor_config.swanlab_config
+        super().__init__(swanlab_config)
+
+        self.enabled = swanlab_config.enabled
+        self.workspace = swanlab_config.workspace
+        self.project = swanlab_config.project
+        self.experiment_name = swanlab_config.experiment_name
+
+        if self.enabled and dist.get_rank() == 0:
+            swanlab.init(project=self.project, workspace=self.workspace, experiment_name=self.experiment_name)
+
+    def update_config(self, args=None, config=None, config_params=None):
+        # Set config using config_params for deepspeed backwards compat
+        if config is None and config_params is not None:
+            config = config_params
+
+        if hasattr(args, "deepscale_config") and args.deepscale_config is not None:
+            args.deepspeed_config = args.deepscale_config
+            args.deepscale_config = None
+        if hasattr(args, "deepspeed_config") and args.deepspeed_config is not None:
+            assert (
+                config is None
+            ), "Not sure how to proceed, we were given deepspeed configs in the deepspeed arguments and deepspeed.initialize() function call"
+            config = args.deepspeed_config
+        dp_config = DeepSpeedConfig(config)
+        swanlab.config.update(dp_config._param_dict)
+
+    def write_events(self, event_list):
+        if self.enabled and dist.get_rank() == 0:
+            for event in event_list:
+                label = event[0]
+                value = event[1]
+                step = event[2]
+                swanlab.log({label: value}, step=step)
